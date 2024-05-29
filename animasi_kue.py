c862b6ec71f1d04c12256a7d99087295e8691e24
SitiJulaika <163138265+SitiJulaika@users.noreply.github.com>
2024-05-29 22:06:37 +0700

    Add files via upload

---
+++ animasi_kue.py
@@ -0,0 +1,25 @@
+import os
+os.system ('cls')
+from termcolor import cprint, colored
+
+print(" " * 13 ,end = " " )
+print(colored(" ^ " ,'red'),end = " " )
+print(" ")
+
+print(" " * 13 ,end = " " )
+print(colored (" | " ,'white'),end = " " )
+print(" ")
+
+print(" " * 8 ,end = " " )
+cprint(" " * 12, 'cyan', 'on_cyan',end = " ") 
+print(" ")
+
+print(" " * 6 ,end = " " )
+cprint(" " * 16 ,'yellow', 'on_yellow',end = " ")
+print(" ")
+
+print(" " * 4 ,end = " " )
+cprint(" " * 20, 'magenta', 'on_magenta',end =" ")
+print(" ")
+
+    
